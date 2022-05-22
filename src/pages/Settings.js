fd7175b65a7411107dee6fc50691b57d20219d52
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-22 11:04:03 +0700

    Mengimpelentasikan component SelectField tadi pada Settings component

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,6 +1,21 @@
+import { Button } from "@mui/material"
+import { Box } from "@mui/system"
+import SelectField from "../components/SelectField"
+
 const Settings = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
   return (
-    <div>Settings</div>
+    <form onSubmit={handleSubmit}>
+      <SelectField label="Category" />
+      <SelectField label="Difficulty" />
+      <SelectField label="Type" />
+      <Box mt={3} width="100%">
+        <Button variant="contained" fullWidth type="submit">Get Started</Button>
+      </Box>
+    </form>
   )
 }
 
