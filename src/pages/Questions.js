a89cf701c7efcdcf171d65ce3af907652000b2f3
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-22 15:47:12 +0700

    Mengatur tampilan pada question pages

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,6 +1,25 @@
+import { Box, Button, Typography } from "@mui/material"
+import useAxios from '../hooks/useAxios'
+
 const Questions = () => {
+  let apiUrl = `/api.php?amount=10`;
+  const { response, error, loading } = useAxios({ url: apiUrl });
+  console.log(response);
+
   return (
-    <div>Questions</div>
+    <Box>
+      <Typography variant="h4">Question 1</Typography>
+      <Typography mt={5}>The question will be shown here</Typography>
+      <Box mt={2}>
+        <Button variant="outlined">Answer 1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="outlined">Answer 2</Button>
+      </Box>
+      <Box mt={5}>
+        Score: 10 / 10
+      </Box>
+    </Box>
   )
 }
 
