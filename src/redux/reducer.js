a6e5b8c654bbaead820b7ddab7b45919190a36d6
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-22 16:12:56 +0700

    Set redux environment

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  questions_category: "",
+  questions_difficulty: "",
+  questions_type: "",
+  amount_of_question: 50,
+  score: 0,
+}
+
+const reducer = ( state = initialState, action ) => {
+  return state;
+}
+
+export default reducer
