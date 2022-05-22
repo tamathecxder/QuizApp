1eaa06aec35a705c45dbbbd15cea314706478785
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-22 10:28:24 +0700

    Membungkus semua routes dan componentnya dengan Materail UI Container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Settings from './pages/Settings';
 import Questions from './pages/Questions';
 import FinalScreen from './pages/FinalScreen';
+import { Box, Container } from "@mui/material";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Settings />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/score" element={<FinalScreen />} />
-      </Routes>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route path="/" element={<Settings />} />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/score" element={<FinalScreen />} />
+          </Routes>
+        </Box>
+      </Container>
     </Router>
   );
 }
